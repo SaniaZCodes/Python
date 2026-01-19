18a5085f0f1b819098c7e2a277e668ccfad8e788
Sania Zafar <saniazafar1030@gmail.com>
2026-01-19 19:15:38 +0500

    Create pip.py

---
+++ Basics/pip.py
@@ -0,0 +1,16 @@
+import camelcase
+
+# Drill 1: The "Installation" Command
+# Goal: Learn how to download a library.
+
+
+# Drill 2: Using an External Module
+# Goal: Use the library you just downloaded.
+
+c = camelcase.CamelCase()
+txt = "hello world bro"
+print(c.hump(txt))
+
+
+# Drill 3: Checking Your Tools
+# Goal: See everything installed on your system.
